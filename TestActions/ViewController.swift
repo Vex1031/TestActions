2222a045e4dd0c568815ec30a263e16a4783f7ea
Vex1031 <103010841+Vex1031@users.noreply.github.com>
2022-04-22 14:56:41 +0900

    ads123

--- TestActions/ViewController.swift
+++ TestActions/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("testdsad")
+        print("testdsaddfsadads")
         // Do any additional setup after loading the view.
     }
 

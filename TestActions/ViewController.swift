c80e9ac45df5fabba553e456e6757a52b044f934
Vex1031 <103010841+Vex1031@users.noreply.github.com>
2022-04-22 17:02:29 +0900

    asd
    asd

--- TestActions/ViewController.swift
+++ TestActions/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("testdsaasasddfasdsaddsd")
+        print("testddsaddsd")
         // Do any additionaup after loading the view.
     }
 
